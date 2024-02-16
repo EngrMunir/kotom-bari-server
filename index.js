64d85548368bc4cf37b3462430d544fde735a078
Munir <smsirajulmonir@gmail.com>
2024-02-16 10:05:18 +0600

    json web token is implemented

--- index.js
+++ index.js
@@ -49,11 +49,18 @@ async function run() {
         return res.status(401).send({message})
       }
       const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
       // next();
     }
 
     // users related api
-    app.get('/users', async(req, verifyToken, res) =>{
+    app.get('/users', verifyToken, async(req, res) =>{
       
       const result = await userCollection.find().toArray();
       res.send(result)
