b5f5a7bb5b74c6ce7b601ac7ffb547f2fc762d20
Munir <smsirajulmonir@gmail.com>
2024-02-16 08:48:41 +0600

    middleware implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -33,8 +34,27 @@ async function run() {
     const reviewCollection = client.db("kotomDb").collection("reviews");
     const cartCollection = client.db("kotomDb").collection("carts");
 
+    // jwt related api
+    app.post('/jwt', async(req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn:'1h'});
+        res.send({ token });
+    })
+
+    // middlewares
+    const verifyToken = (req, res, next)=>{
+      console.log('inside verify token',req.headers);
+      if(!req.headers.authorization){
+        return res.status(401).send({message})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      // next();
+    }
+
     // users related api
-    app.get('/users', async(req, res) =>{
+    app.get('/users', async(req, verifyToken, res) =>{
+      
       const result = await userCollection.find().toArray();
       res.send(result)
     })
@@ -51,6 +71,25 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role:'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async(req, res) =>{
+      const id=req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+    // menu related apis
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
         res.send(result);
