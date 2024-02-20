33becf587d6c11835c9640103b0dce452bf937fd
Munir <smsirajulmonir@gmail.com>
2024-02-20 22:55:44 +0600

    specific user payment history get api ready

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ async function run() {
     const menuCollection = client.db("kotomDb").collection("menu");
     const reviewCollection = client.db("kotomDb").collection("reviews");
     const cartCollection = client.db("kotomDb").collection("carts");
+    const paymentCollection = client.db("kotomDb").collection("payments");
 
     // jwt related api
     app.post('/jwt', async(req, res) =>{
@@ -198,6 +199,45 @@ async function run() {
       // console.log(result);
     })
 
+    // payment intent
+    app.post('/create-payment-intent', async(req, res) =>{
+      const {price}= req.body;
+      const amount = parseInt(price*100);
+      
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types:['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+    app.get('/payments/:email',verifyToken, async(req, res)=>{
+      const query ={ email: req.params.email }
+      if(req.params.email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/payments', async(req, res)=>{
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      // carefully delete each item from the cart
+      console.log('payment info ', payment);
+      const query ={_id:{
+        $in: payment.cartIds.map(id => new ObjectId(id))
+      }};
+      const deleteResult = await cartCollection.deleteMany(query);
+      console.log('delete result', deleteResult);
+      console.log('payment result ', paymentResult);
+
+      res.send({paymentResult, deleteResult});
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
